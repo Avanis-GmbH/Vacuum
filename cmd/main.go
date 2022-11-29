cf9471935216564cdab965db28f9e27268f96f39
HighQualityWaschbaer <simonrach@outlook.com>
2022-11-29 16:41:25 +0100

    [ADD] Added project structure.

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+const VERSION = "0.0.1"
+
 func main() {
+
+	showStartupBanner()
+
 	fmt.Println("Hewo UwU")
+}
+
+func showStartupBanner() {
+	fmt.Println("=============================================================")
+	fmt.Printf("Go Dust Vacuum v%v - Created by Simon Nils Rach \n", VERSION)
+	fmt.Printf("=============================================================\n\n")
 }
