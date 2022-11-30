dc7e597978173d873ed69ecf344b7301f734f309
HighQualityWaschbaer <simonrach@outlook.com>
2022-11-30 16:23:21 +0100

    [ADD] Added start of clean funtion.

--- pkg/vacuum/types.go
+++ pkg/vacuum/types.go
@@ -3,9 +3,11 @@ package vacuum
 type OperationStats struct {
 	CopiedFiles uint
 	CopiedBytes uint
+	Errors      uint
 }
 
 func (o *OperationStats) Add(co *OperationStats) {
 	o.CopiedFiles += co.CopiedFiles
 	o.CopiedBytes += co.CopiedBytes
+	o.Errors += co.Errors
 }
--- pkg/vacuum/vacuum.go
+++ pkg/vacuum/vacuum.go
@@ -1,1 +1,61 @@
 package vacuum
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+var RECURSIVE, DRY_RUN, SHRED_ORIGINAL, NO_PROTOCOL bool
+var MIN_AGE_IN_YEARS = 11
+var TARGET_DIR string
+
+func Clean(rootDir, branchDir string) (*OperationStats, error) {
+	entries, err := os.ReadDir(filepath.Join(rootDir, branchDir))
+	if err != nil {
+		return nil, err
+	}
+
+	stats := &OperationStats{}
+
+	for _, f := range entries {
+
+		if f.IsDir() {
+
+			fmt.Printf("Found directory %v in %v \n", f.Name(), filepath.Join(rootDir, branchDir))
+
+			if RECURSIVE {
+				stat, err := Clean(rootDir, filepath.Join(branchDir, f.Name()))
+				if err != nil {
+					//TODO LOG
+					stats.Errors++
+					continue
+				}
+
+				stats.Add(stat)
+			}
+
+			continue
+		}
+
+		fInfo, err := f.Info()
+		if err != nil {
+			//TODO log
+			stats.Errors++
+			continue
+		}
+
+		fmt.Printf("Found file %v in %v \n", fInfo.Name(), filepath.Join(rootDir, branchDir))
+
+		if fInfo.ModTime().Year() > time.Now().Year()-MIN_AGE_IN_YEARS {
+			continue
+		}
+
+		fmt.Printf("File %v in %v is older than %v years: %v", fInfo.Name(), filepath.Join(rootDir, branchDir), fmt.Sprint(MIN_AGE_IN_YEARS), fInfo.ModTime())
+
+		//TODO rest
+	}
+
+	return stats, nil
+}
