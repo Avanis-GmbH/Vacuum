5c1c778ce9abeebe131ad2c3d576fd1531e26a28
HighQualityWaschbaer <simonrach@outlook.com>
2022-11-30 16:34:38 +0100

    [ADD] Added Logger interface.

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -1,1 +1,9 @@
 package logging
+
+import "os"
+
+type Logger interface {
+	LogOldFile(f *os.FileInfo, maxAgeInYears uint) error
+	LogCopiedFile(f *os.FileInfo, originPath, copyPath string) error
+	LogShreddedFile(f *os.FileInfo, originPath string, err error) error
+}
