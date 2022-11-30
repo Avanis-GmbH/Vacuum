92259601aba3fb332f9d60d49c2bf4ada4d6a91f
HighQualityWaschbaer <simonrach@outlook.com>
2022-11-30 16:35:25 +0100

    [ADD] Implemented NoLogger type.

--- pkg/nologger/nologger.go
+++ pkg/nologger/nologger.go
@@ -1,1 +1,17 @@
 package nologger
+
+import "os"
+
+type NoLogger struct{}
+
+func (nl *NoLogger) LogOldFile(f *os.FileInfo, maxAgeInYears uint) error {
+	return nil
+}
+
+func (nl *NoLogger) LogCopiedFile(f *os.FileInfo, originPath, copyPath string) error {
+	return nil
+}
+
+func (nl *NoLogger) LogShreddedFile(f *os.FileInfo, originPath string, err error) error {
+	return nil
+}
