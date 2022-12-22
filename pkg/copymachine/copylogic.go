bc0e8e5752a5a5215f06db507faf4be2733277cb
HighQualityWaschbaer <simonrach@outlook.com>
2022-12-22 13:49:48 +0100

    [FIX] Fixed destination path not existing.

--- pkg/copymachine/copylogic.go
+++ pkg/copymachine/copylogic.go
@@ -1,8 +1,10 @@
 package copymachine
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 func (cm *CopyMachine) copyQueueMasterRoutine() {
@@ -13,6 +15,7 @@ func (cm *CopyMachine) copyQueueMasterRoutine() {
 
 	for jobAmount > 0 {
 		cm.copyJobStackMutes.Lock()
+		jobAmount = len(cm.copyJobs)
 		cj := cm.copyJobs[0]
 
 		if jobAmount == 1 {
@@ -33,8 +36,18 @@ func (cm *CopyMachine) copyQueueMasterRoutine() {
 
 func (cm *CopyMachine) performCopyJob(cj *CopyJob) {
 
+	fmt.Printf("Performing copyjob %+v \n", cj)
+
 	// Abort without any errors when doing a dry run
 	if cm.Dry {
+		cj.FinishCallBack(cj)
+		return
+	}
+
+	// Create the target directory if it does not exist
+	err := os.MkdirAll(filepath.Dir(*cj.ToPath), 0666)
+	if err != nil {
+		cj.CopyError = &err
 		cj.FinishCallBack(cj)
 		return
 	}
@@ -70,6 +83,12 @@ func (cm *CopyMachine) performCopyJob(cj *CopyJob) {
 	err = destF.Sync()
 	if err != nil {
 		cj.CopyError = &err
+	}
+
+	// Close the source file
+	err = sourceF.Close()
+	if err != nil {
+		fmt.Printf("Error while closing file after copy: %v \n", err.Error())
 	}
 
 	cj.FinishCallBack(cj)
